40a2c5996f1c112b35bad0e50742cab1e1ed9142
Alan <alandsantos54@gmail.com>
2025-08-09 13:56:42 -0300

    DevClub Elite - Elite Traker pt.2 - aula 04

---
+++ src/routes.ts
@@ -0,0 +1,22 @@
+import { response, Router } from "express";
+
+import packageJson from "../package.json";
+
+export const routes = Router();
+
+const habits = [];
+
+routes.get("/", (request, response) => {
+	const { name, description, version } = packageJson;
+	return response.status(200).json({ name, description, version });
+});
+
+routes.post("/habits", (request, response) => {
+	const { name } = request.body;
+
+	const newHabit = { name };
+
+	habits.push(newHabit);
+
+	return response.status(200).json(newHabit);
+});
