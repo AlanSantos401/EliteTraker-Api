5b13e59e08323c36dbee0357314bda579fcd1d26
Alan <alandsantos54@gmail.com>
2025-08-09 16:02:48 -0300

    DevClub Elite - Elite Traker pt.2 - Aula 10

--- src/controllers/habits.controller.ts
+++ src/controllers/habits.controller.ts
@@ -1,13 +1,36 @@
 import { Request, Response } from "express";
+import { z } from 'zod'
 import { habitModel } from "../schemas/habit.model";
 
 export class HabitsController {
 	private readonly habits: any[] = [];
 	store = async (request: Request, response: Response): Promise<Response> => {
+		const schema = z.object({
+			name: z.string(),
+		})
+
 		const { name } = request.body;
 
-		const newHabit = await habitModel.create({
+		const habit = schema.safeParse({
 			name,
+		})
+
+		if (!habit.success) {
+			return response.status(400).json({
+				message: "Error on validation."
+			});
+		}
+
+		const findHabit = await habitModel.findOne({
+			name: habit.data.name
+		})
+
+		if (findHabit) {
+			return response.status(400).json({ message: "Habit already exists." })
+		}
+
+		const newHabit = await habitModel.create({
+			name: habit.data.name,
 			completedDates: []
 		})
 
